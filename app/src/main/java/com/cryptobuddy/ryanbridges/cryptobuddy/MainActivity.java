9c1890c868616fdc88c0b8e808b88f6b09ccf531
Ryan <rybridges16@gmail.com>
2017-12-27 23:09:33 -0800

    Adding a couple of currencies

--- app/src/main/java/com/cryptobuddy/ryanbridges/cryptobuddy/MainActivity.java
+++ app/src/main/java/com/cryptobuddy/ryanbridges/cryptobuddy/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
     private static final String NEWS_API_KEY = BuildConfig.API_KEY;
     public final static String BTC_NEWS_URL_TEMPLATE = "http://eventregistry.org/json/article?query=%7B\"%24query\"%3A%7B\"%24and\"%3A%5B%7B\"conceptUri\"%3A%7B\"%24and\"%3A%5B\"http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FEthereum\"%2C\"http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCryptocurrency\"%2C\"http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBitcoin\"%2C\"http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FLitecoin\"%5D%7D%7D%2C%7B\"lang\"%3A\"eng\"%7D%5D%7D%7D&action=getArticles&resultType=articles&articlesSortBy=date&articlesCount=20&apiKey=";
-    private String homeCurrencyListURL = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,ETC,XRP,XMR,DASH,BCH,BTG&tsyms=USD";
+    private String homeCurrencyListURL = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,ETC,XRP,XMR,DASH,BCH,BTG,XLM,XVG,XRB,SONM&tsyms=USD";
     public final static String BTC_NEWS_URL = BTC_NEWS_URL_TEMPLATE + NEWS_API_KEY;
     private String TAG = MainActivity.class.getSimpleName();
     public final static String SYMBOL = "SYMBOL";
