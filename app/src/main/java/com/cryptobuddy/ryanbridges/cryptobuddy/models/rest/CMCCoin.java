d75afaf871c5bbdae8a4c7fcf01a6bd518c30dd2
Patchett <rybridges16@gmail.com>
2018-12-15 13:57:02 -0800

    Switching new new APIs

--- app/src/main/java/com/cryptobuddy/ryanbridges/cryptobuddy/models/rest/CMCCoin.java
+++ app/src/main/java/com/cryptobuddy/ryanbridges/cryptobuddy/models/rest/CMCCoin.java
@@ -5,6 +5,8 @@ import android.os.Parcelable;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 /**
  * Created by Ryan on 1/16/2018.
  */
@@ -14,6 +16,9 @@ public class CMCCoin implements Parcelable {
     public CMCCoin() {
 
     }
+
+    @JsonProperty("data")
+    List<CMCCoinProData> coinsList;
 
     public CMCCoin(Parcel in){
         String[] data = new String[16];
@@ -74,164 +79,159 @@ public class CMCCoin implements Parcelable {
                 Integer.toString(this.quickSearchID)});
     }
 
-    private int quickSearchID;
-    //Side note: the names don't need to match. The annotation name is the one used to serialize the property.
-    @JsonProperty("id")
-    private String id;
-    @JsonProperty("name")
-    private String name;
-    @JsonProperty("symbol")
-    private String symbol;
-    @JsonProperty("rank")
-    private String rank;
-    @JsonProperty("price_usd")
-    private String price_usd;
-    @JsonProperty("price_btc")
-    private String price_btc;
-    @JsonProperty("24h_volume_usd")
-    private String volume_usd_24h;
-    @JsonProperty("market_cap_usd")
-    private String market_cap_usd;
-    @JsonProperty("available_supply")
-    private String available_supply;
-    @JsonProperty("total_supply")
-    private String total_supply;
-    @JsonProperty("max_supply")
-    private String max_supply;
-    @JsonProperty("percent_change_1h")
-    private String percent_change_1h;
-    @JsonProperty("percent_change_24h")
-    private String percent_change_24h;
-    @JsonProperty("percent_change_7d")
-    private String percent_change_7d;
-    @JsonProperty("last_updated")
-    private String last_updated;
+    public class CMCCoinProData {
+        @JsonProperty("id")
+        private String id;
+        @JsonProperty("name")
+        private String name;
+        @JsonProperty("symbol")
+        private String symbol;
+        @JsonProperty("slug")
+        private String slug;
+        @JsonProperty("circulating_supply")
+        private long circulating_supply;
+        @JsonProperty("total_supply")
+        private long total_supply;
+        @JsonProperty("max_supply")
+        private long max_supply;
+        @JsonProperty("cmc_rank")
+        private long cmc_rank;
+        @JsonProperty("quote")
+        private CMCQuote quote;
 
-    public String getId() {
-        return id;
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public String getSlug() {
+            return slug;
+        }
+
+        public void setSlug(String slug) {
+            this.slug = slug;
+        }
+
+        public long getCirculating_supply() {
+            return circulating_supply;
+        }
+
+        public void setCirculating_supply(long circulating_supply) {
+            this.circulating_supply = circulating_supply;
+        }
+
+        public long getTotal_supply() {
+            return total_supply;
+        }
+
+        public void setTotal_supply(long total_supply) {
+            this.total_supply = total_supply;
+        }
+
+        public long getMax_supply() {
+            return max_supply;
+        }
+
+        public void setMax_supply(long max_supply) {
+            this.max_supply = max_supply;
+        }
+
+        public long getCmc_rank() {
+            return cmc_rank;
+        }
+
+        public void setCmc_rank(long cmc_rank) {
+            this.cmc_rank = cmc_rank;
+        }
+
+        public CMCQuote getQuote() {
+            return quote;
+        }
+
+        public void setQuote(CMCQuote quote) {
+            this.quote = quote;
+        }
     }
 
-    public String getName() {
-        return name;
-    }
+    public class CMCQuote {
+        @JsonProperty("price")
+        private double price;
+        @JsonProperty("volume_24h")
+        private double volume_24h;
+        @JsonProperty("percent_change_1h")
+        private double percent_change_1h;
+        @JsonProperty("percent_change_24h")
+        private double percent_change_24h;
+        @JsonProperty("percent_change_7d")
+        private double percent_change_7d;
+        @JsonProperty("market_cap")
+        private double market_cap;
 
-    public String getSymbol() {
-        return symbol;
-    }
+        public double getPrice() {
+            return price;
+        }
 
-    public String getRank() {
-        return rank;
-    }
+        public void setPrice(double price) {
+            this.price = price;
+        }
 
-    public String getPrice_usd() {
-        return price_usd;
-    }
+        public double getVolume_24h() {
+            return volume_24h;
+        }
 
-    public String getPrice_btc() {
-        return price_btc;
-    }
+        public void setVolume_24h(double volume_24h) {
+            this.volume_24h = volume_24h;
+        }
 
-    public String getVolume_usd_24h() {
-        return volume_usd_24h;
-    }
+        public double getPercent_change_1h() {
+            return percent_change_1h;
+        }
 
-    public String getMarket_cap_usd() {
-        return market_cap_usd;
-    }
+        public void setPercent_change_1h(double percent_change_1h) {
+            this.percent_change_1h = percent_change_1h;
+        }
 
-    public String getAvailable_supply() {
-        return available_supply;
-    }
+        public double getPercent_change_24h() {
+            return percent_change_24h;
+        }
 
-    public String getTotal_supply() {
-        return total_supply;
-    }
+        public void setPercent_change_24h(double percent_change_24h) {
+            this.percent_change_24h = percent_change_24h;
+        }
 
-    public String getMax_supply() {
-        return max_supply;
-    }
+        public double getPercent_change_7d() {
+            return percent_change_7d;
+        }
 
-    public String getPercent_change_1h() {
-        return percent_change_1h;
-    }
+        public void setPercent_change_7d(double percent_change_7d) {
+            this.percent_change_7d = percent_change_7d;
+        }
 
-    public String getPercent_change_24h() {
-        return percent_change_24h;
-    }
+        public double getMarket_cap() {
+            return market_cap;
+        }
 
-    public String getPercent_change_7d() {
-        return percent_change_7d;
-    }
-
-    public String getLast_updated() {
-        return last_updated;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public void setPrice_usd(String price_usd) {
-        this.price_usd = price_usd;
-    }
-
-    public void setPrice_btc(String price_btc) {
-        this.price_btc = price_btc;
-    }
-
-    public void setVolume_usd_24h(String volume_usd_24h) {
-        this.volume_usd_24h = volume_usd_24h;
-    }
-
-    public void setMarket_cap_usd(String market_cap_usd) {
-        this.market_cap_usd = market_cap_usd;
-    }
-
-    public void setAvailable_supply(String available_supply) {
-        this.available_supply = available_supply;
-    }
-
-    public void setTotal_supply(String total_supply) {
-        this.total_supply = total_supply;
-    }
-
-    public void setMax_supply(String max_supply) {
-        this.max_supply = max_supply;
-    }
-
-    public void setPercent_change_1h(String percent_change_1h) {
-        this.percent_change_1h = percent_change_1h;
-    }
-
-    public void setPercent_change_24h(String percent_change_24h) {
-        this.percent_change_24h = percent_change_24h;
-    }
-
-    public void setPercent_change_7d(String percent_change_7d) {
-        this.percent_change_7d = percent_change_7d;
-    }
-
-    public void setLast_updated(String last_updated) {
-        this.last_updated = last_updated;
-    }
-
-    public void setQuickSearchID(int quickSearchID) {
-        this.quickSearchID = quickSearchID;
-    }
-
-    public int getQuickSearchID() {
-        return quickSearchID;
+        public void setMarket_cap(double market_cap) {
+            this.market_cap = market_cap;
+        }
     }
 }
--- app/src/main/java/com/cryptobuddy/ryanbridges/cryptobuddy/rest/CoinMarketCapService.java
+++ app/src/main/java/com/cryptobuddy/ryanbridges/cryptobuddy/rest/CoinMarketCapService.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import com.cryptobuddy.ryanbridges.cryptobuddy.currencydetails.chartandtable.GraphFragment;
 import com.cryptobuddy.ryanbridges.cryptobuddy.models.rest.CMCChartData;
 import com.cryptobuddy.ryanbridges.cryptobuddy.models.rest.CMCCoin;
-import com.cryptobuddy.ryanbridges.cryptobuddy.models.rest.CMCQuickSearch;
 import com.grizzly.rest.GenericRestCall;
 import com.grizzly.rest.Model.afterTaskCompletion;
 import com.grizzly.rest.Model.afterTaskFailure;
@@ -19,14 +18,14 @@ import org.springframework.http.HttpMethod;
 
 public class CoinMarketCapService {
 
+    public static final String PRO_CMC_ALL_COINS_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=market_cap&CMC_PRO_API_KEY=9de23807-460a-4991-9389-aaea74b440b8";
     public static final String COIN_MARKETCAP_ALL_COINS_URL = "https://api.coinmarketcap.com/v1/ticker/?limit=0";
     public static final String COIN_MARKETCAP_CHART_URL_ALL_DATA = "https://graphs2.coinmarketcap.com/currencies/%s/";
     public static final String COIN_MARKETCAP_CHART_URL_WINDOW = "https://graphs2.coinmarketcap.com/currencies/%s/%s/%s/";
-    public static final String COIN_MARKETCAP_QUICK_SEARCH_URL = "https://s2.coinmarketcap.com/generated/search/quick_search.json";
 
-    public static void getAllCoins(Context context, afterTaskCompletion<CMCCoin[]> taskCompletion, afterTaskFailure failure, boolean async) {
-        new GenericRestCall<>(Void.class, CMCCoin[].class, String.class)
-                .setUrl(COIN_MARKETCAP_ALL_COINS_URL)
+    public static void getAllCoins(Context context, afterTaskCompletion<CMCCoin> taskCompletion, afterTaskFailure failure, boolean async) {
+        new GenericRestCall<>(Void.class, CMCCoin.class, String.class)
+                .setUrl(PRO_CMC_ALL_COINS_URL)
                 .setContext(context.getApplicationContext())
                 .isCacheEnabled(true)
                 .setCacheTime(300000L)
@@ -48,19 +47,4 @@ public class CoinMarketCapService {
                 .setTaskFailure(failure)
                 .setAutomaticCacheRefresh(false).execute(async);
     }
-
-    public static void getCMCQuickSearch(Context context, afterTaskCompletion<CMCQuickSearch[]> taskCompletion, afterTaskFailure failure, boolean async) {
-        Log.d("I", "Getting URL in getCMCQuickSearch: " + COIN_MARKETCAP_QUICK_SEARCH_URL);
-        new GenericRestCall<>(Void.class, CMCQuickSearch[].class, String.class)
-                .setUrl(COIN_MARKETCAP_QUICK_SEARCH_URL)
-                .setContext(context.getApplicationContext())
-                .isCacheEnabled(true)
-                // cache for 6 hours
-                .setCacheTime(21600000L)
-                .setMethodToCall(HttpMethod.GET)
-                .setTaskCompletion(taskCompletion)
-                .setTaskFailure(failure)
-                .setAutomaticCacheRefresh(true).execute(async);
-    }
-
 }
